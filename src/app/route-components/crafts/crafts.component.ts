d0faccb0a697841b20d926670b098aa260b2c8ce
Brian Beeler <brianlbeeler@gmail.com>
2019-08-09 17:10:50 -0500

    updated photos

--- src/app/route-components/crafts/crafts.component.ts
+++ src/app/route-components/crafts/crafts.component.ts
@@ -12,7 +12,14 @@ export class CraftsComponent implements OnInit {
   imageUrlArray = [
     '/assets/images/kaite-table-3.jpg',
     '/assets/images/katie-feenie-table.jpg',
-    '/assets/images/katie-table-2.jpg'
+    '/assets/images/katie-table-2.jpg',
+    '/assets/images/be_still.jpg',
+    '/assets/images/kentucky_map.jpg',
+    '/assets/images/my_foundation.jpg',
+    '/assets/images/beeler_squares.jpg',
+    '/assets/images/uk_coasters.jpg',
+    '/assets/images/wildcat_country.jpg',
+    '/assets/images/uk_painting.jpg'
   ];
 
   ngOnInit() {
