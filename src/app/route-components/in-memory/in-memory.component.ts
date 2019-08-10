2e1acd8054996bc37fe904250ccc80b8b7031e21
Brian Beeler <brianlbeeler@gmail.com>
2019-08-10 11:31:13 -0500

    Added a picture of Molly.

--- src/app/route-components/in-memory/in-memory.component.ts
+++ src/app/route-components/in-memory/in-memory.component.ts
@@ -11,7 +11,9 @@ export class InMemoryComponent implements OnInit {
 
   inMemoryImages: string[] = [
     'assets/images/mini-golf.jpg',
-    'assets/images/grandads_shop.jpg'
+    'assets/images/grandads_shop.jpg',
+    'assets/images/emily_photo.jpg',
+    'assets/images/molly_harlan.jpg'
   ];
 
   ngOnInit() {
