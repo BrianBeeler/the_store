4559500e3254cafef81b8db627629e04cd57177d
Brian Beeler <brianlbeeler@gmail.com>
2019-08-10 12:25:46 -0500

    Added captions.

--- src/app/route-components/in-memory/in-memory.component.ts
+++ src/app/route-components/in-memory/in-memory.component.ts
@@ -11,7 +11,6 @@ export class InMemoryComponent implements OnInit {
 
   inMemoryPhotos: string[] = [
     'assets/images/mini-golf.jpg',
-    'assets/images/grandads_shop.jpg',
     'assets/images/emily_photo.jpg',
     'assets/images/molly_harlan.jpg'
   ];
